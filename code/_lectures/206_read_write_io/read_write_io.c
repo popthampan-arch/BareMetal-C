096f0fdc98bbb4a51690cd520024b3f0ec80b42f
kongkrit <kongkrit@gmail.com>
2025-12-29 01:37:32 +0700

    Update read_write_io.c

--- code/_lectures/206_read_write_io/read_write_io.c
+++ code/_lectures/206_read_write_io/read_write_io.c
@@ -2,15 +2,15 @@
 #include <stdint.h>
 
 #define LED0    ((volatile bool *)0xE000U)
-#define SWITCH0 ((volatile bool *)0xC000U)
+#define TOGGLE0 ((volatile bool *)0xC000U)
 
 void main(void) {
 
     volatile bool *led0    = LED0;
-    volatile bool *switch0 = SWITCH0;
+    volatile bool *toggle0 = TOGGLE0;
 
     while (true) {
-        if (*switch0) {
+        if (*toggle0) {
             *led0 = false;
         } else {
             *led0 = true;

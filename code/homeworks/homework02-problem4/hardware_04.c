cd12c5071fcf90d2976a95e80131440968c3e329
popthampan-arch <popthampan@gmail.com>
2026-02-01 18:00:04 +0700

    Update hardware_04.c

--- code/homeworks/homework02-problem4/hardware_04.c
+++ code/homeworks/homework02-problem4/hardware_04.c
@@ -2,15 +2,51 @@
 #include <stdint.h>
 
 #define LED0 ((volatile bool *)0xE000U)
+#define LED1 ((volatile bool *)0xE001U)
+#define LED3 ((volatile bool *)0xE003U)
+#define COUNTER (volatile uint16_t *)0x4FF0
+void delay(uint16_t n) {
+    uint16_t c;
+    *COUNTER = 0;
+    c = 0;
+    while (c < n) {
+        c = c + 1;
+    }
+}
 
 void main(void) {
 
-    volatile bool *led; 
-    led = LED0;
+    volatile bool *led0; 
+    volatile bool *led1; 
+    volatile bool *led3; 
+    led0 = LED0;
+    led1 = LED1;
+    led3 = LED3;
 
     while (true) {
-        *led = true;
-        *led = false;
+        *led1 = true;
+        delay(1500);
+        *led1 = false;
+
+        *led3 = true;
+        delay(1500);
+        *led3 = false;
+
+        *led0 = true;
+        delay(1500);
+        *led0 = false;
+
+        *led3 = true;
+        delay(1500);
+        *led3 = false;
+
+        *led0 = true;
+        delay(1500);
+        *led0 = false;
+
+        *led3 = true;
+        delay(1500);
+        *led3 = false;
     }
 
 }
